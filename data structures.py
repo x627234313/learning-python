7f91020af86265aa2effc9ea90587c3fa2924901
Chai <x627234313@163.com>
2017-07-26 20:04:36 +0800

    Update data structures.py

--- data structures.py
+++ data structures.py
@@ -31,7 +31,7 @@ print(list1)
 print(list2)
 
 #整数
-i = 12,i = int(3.14), i = int('0b100', base=0)    # 4
+i = 12, i = int(3.14), i = int('0b100', base=0)    # 4
 """
 bit_length()  # Number of bits necessary to represent self in binary. 返回整数的二进制的bit位数
 conjugate()   # Returns self, the complex conjugate of any int.
@@ -44,3 +44,24 @@ real          # the real part of a complex number. 一个复数的实数部分
 denominator   # the denominator of a rational number in lowest terms. 分母有理数的
 numerator     # the numerator of a rational number in lowest terms. 分子有理数的
 """
+
+
+#浮点数
+f = 3.14, f = float(12)
+"""
+as_integer_ratio()    # Return a pair of integers, whose ratio is exactly equal to the original 
+                      # float and with a positive denominator. 将浮点数表示为两个整数的商。
+                      # float.as_integer_ratio() -> (int, int)
+                      # Raise OverflowError on infinities and a ValueError on NaNs.
+conjugate()           # Return self, the complex conjugate of any float.
+fromhex()             # Create a floating-point number from a hexadecimal string. 16进制字符串转换为浮点数。
+                      # float.fromhex(string) -> float 
+hex()                 # Return a hexadecimal representation of a floating-point number.
+                      # float.hex() -> string
+is_integer()          # Return True if the float is an integer.
+imag                  #
+real                  #
+"""
+
+
+#复数
