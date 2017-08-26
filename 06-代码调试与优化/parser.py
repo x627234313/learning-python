d99a1e165330f9dc5e0d403828eae79d79f8d642
chai <x627234313@163.com>
2017-08-26 16:23:58 +0800

    Update parser.py

--- 06-代码调试与优化/parser.py
+++ 06-代码调试与优化/parser.py
@@ -6,9 +6,10 @@ import yaml
 
 cfg = ConfigParser()
 cfg.read('config.ini')
-print(cfg.sections())
-print(cfg.get('Common', 'version'))
+print(cfg.sections())    # ['Chrome', 'IE', 'Common', 'SmartRepair']
+print(cfg.get('Common', 'version'))    # 2.0.0.88
 
 f = open('config.yaml')
 content = yaml.load(f)
-print(content)
+print(content)    # {'name': 'junxi', 'age': 18, 'spouse': {'name': 'Rui', 'age': 18}, 
+                  #  'children': [{'name': 'Chen You', 'age': 3}, {'name': 'Ruo Xi', 'age': 2}]}
